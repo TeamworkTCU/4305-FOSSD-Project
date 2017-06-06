d6fbf696cadb53db8f5b7205f75bfb986ad10611
TrinhDinhPhuc <phuccoi996@gmail.com>
2017-06-06 17:42:51 +0700

    Cons

--- Cons.cs
+++ Cons.cs
@@ -4,18 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calender
+namespace Calendar_PT
 {
     class Cons
     {
         public static int DayOfWeek = 7;
         public static int DayOfColumn = 6;
-
-        public static int dateButtonWidth = 75;
-        public static int dateButtonHeight = 40;
-
-        public static int margin = 6;
-        public static int notifyTime = 1;
-        public static int notifyTimeOut = 10000;
+        public static int dateButtonWidth = 57;
+        public static int dateButtonHeight = 30;
+        public static int margin = 15;
     }
 }
