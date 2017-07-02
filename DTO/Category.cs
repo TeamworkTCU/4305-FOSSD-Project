8c17f60cc9a2288dd556772d2c78334e9e311916
TrinhDinhPhuc <phuccoi996@gmail.com>
2017-07-01 18:38:23 -0700

    Bill + Bill info + Category

---
+++ DTO/Category.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanCafe.DTO
+{
+    public class Category
+    {
+        public Category(int id, string name)
+        {
+            this.ID = id;
+            this.Name = name;
+        }
+
+        public Category(DataRow row)
+        {
+            this.ID = (int)row["id"];
+            this.Name = row["name"].ToString();
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int iD;
+
+        public int ID
+        {
+            get { return iD; }
+            set { iD = value; }
+        }
+    }
+}
